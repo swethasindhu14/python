e6525076ee5246a1dd204df9266b5cd96b2c1bd5
swethasindhu14 <53964040+swethasindhu14@users.noreply.github.com>
2019-08-10 12:32:52 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,25 @@
-n=int(input("enter a numbe
+x=int(input("enter a numbe"))
+price=int(input('enter price of coffee'))
+p=int(input("enter discount"))
+for i in range(n):
+  if i==1:
+      price=(price/p)*100
+      print('price of the coffee',price)
+      
+  if keepgoing=="y":
+      
+      if i==2:
+      price=(price/p)*100
+      print('price of the coffee",price)
+    if i==3:
+      price=(price/p)*100
+      print('price of the coffee",price)
+    if i==4:
+      print("the coffee is free")
+      price=(price/p)*100
+  print('price of the coffee",price)
+        
+            
+  
+  
+
